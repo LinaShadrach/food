8d61aa0131021a95c616a10f59c47dfa0b810626
Jonathan Thom and Lina Shadrach <jonathan@>
2016-10-18 09:31:25 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+  $("#section1 h2").click(function() {
+    $("#section1 p").toggle();
+  });
+
+  $("#section2 h2").click(function() {
+    $("#section2 p").toggle();
+  });
+
+  $("#section3 h2").click(function() {
+    $("#section3 p").toggle();
+  });
+});
