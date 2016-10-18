c894a9a33eba4d67860136183edbedb64836a5dd
Jonathan Thom and Lina Shadrach <jonathan@>
2016-10-18 10:33:48 -0700

    add highlighted text, clickable textcursor hover

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,19 @@ $(function() {
     $("#section3 p").toggle();
   });
 
+  $("#section1 p").click(function() {
+    $("#section1 p").addClass("highlight");
+  });
+  $("#section2 p").click(function() {
+    $("#section2 p").addClass("highlight");
+  });
+  $("#section3 p").click(function() {
+    $("#section3 p").addClass("highlight");
+  });
+  
   $(".daybtn").click(function() {
     $("body").addClass("nightmode");
   });
-
   $(".nightbtn").click(function() {
     $("body").removeClass("nightmode");
   });
